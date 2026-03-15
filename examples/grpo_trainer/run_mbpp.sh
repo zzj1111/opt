13a5e3677beb6cfcf640d3b233c2233b8dd3b28d
zzj1111 <731518734@qq.com>
2026-03-14 23:13:03 -0500

    fix: reduce MBPP batch sizes to fit 120-sample train set

--- examples/grpo_trainer/run_mbpp.sh
+++ examples/grpo_trainer/run_mbpp.sh
@@ -95,7 +95,7 @@ python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=$DATA_DIR/train.parquet \
     data.val_files=$DATA_DIR/test.parquet \
-    data.train_batch_size=512\
+    data.train_batch_size=64 \
     data.max_prompt_length=1024 \
     data.max_response_length=3072 \
     data.filter_overlong_prompts=True \
@@ -105,7 +105,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.optim.betas=[0.9,0.999] \
     actor_rollout_ref.actor.checkpoint.save_contents='["hf_model"]' \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=128 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=32 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=16 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
